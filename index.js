b7d2a00fb5e7a56207d6cf5c2ce1217fb7e41711
pankaj marghade <p.g.marghade@gmail.com>
2020-02-07 23:12:46 +0530

    implemented authentication using auth

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
+
 
 
 const port = process.env.PORT || 3000;
@@ -32,6 +34,8 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
+
 
 
 
---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const {User} = require('../models/users');
+const bcrypt = require('bcrypt');
+const Joi = require('@hapi/joi');
+
+// Registering the user
+
+router.post('/', async (req, res) => {
+
+  const result = validate(req.body);
+  if(result.error)
+    return res.status(400).send(result.error);
+
+  let user = await User.findOne({email: req.body.email})
+  if(!user)
+    return res.status(400).send('Wrong username or password');
+
+  const isValid = await bcrypt.compare(req.body.password, user.password)
+  if(!isValid)
+    return res.status(400).send('Wrong username or password');
+  
+  return res.send(true);
+
+})
+
+function validate(body){
+  const userSchema = {
+    email : Joi.string().required().email(),
+    password : Joi.string().required()
+  }
+
+  const joiSchema = Joi.object(userSchema);
+  return joiSchema.validate(body);
+}
+
+module.exports = router;
