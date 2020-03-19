3612f3cb872f3a191143f378bc9fe56bc4e1db26
pankaj marghade <p.g.marghade@gmail.com>
2020-03-19 17:29:25 +0530

    prepared app for integration tests

--- startup/database.js
+++ startup/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const logger = require('./logging').logger;
+const config = require('config');
 
 module.exports = function(){
-  mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true})
-  .then(() => logger.info('connected to vidly database...'));
+  const db = config.get('db');
+  mongoose.connect(db, { useNewUrlParser: true , useUnifiedTopology: true, useCreateIndex: true})
+  .then(() => logger.info(`connected to ${db}...`));
 }
