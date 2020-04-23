f1f5b28c58e200e4ba2f3e819511645bf2ee0bba
pankaj marghade <p.g.marghade@gmail.com>
2020-04-23 19:09:32 +0530

    prepared app for deployment

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('./startup/routes')(app);
 require('./startup/database')();
 require('./startup/config')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 
