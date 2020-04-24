5091578502e84cfb0c7a1330d87b55287cdeace6
pankaj marghade <p.g.marghade@gmail.com>
2020-04-24 15:09:21 +0530

    fixed bugs

--- startup/config.js
+++ startup/config.js
@@ -1,7 +1,7 @@
 const config = require('config');
 
 module.exports = function () {
-  if(!config.get('vidly_jwtPrivateKey')){
+  if(!config.get('jwtPrivateKey')){
     throw new Error('FATAL : private key is not defined');
   }
 }
